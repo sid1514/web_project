1b69a36cc731a37ec6cf13305dab5d804ffd38ab
Siddharth Santosh Shinde <142812176+sid1514@users.noreply.github.com>
2024-02-03 16:22:39 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-=======
-import Footer from './components/Footer';
-import './App.css';
-import Home from './components/Home';
->>>>>>> d9a5711b5bc66ff8740e0970818a6cbf4939aac1
 
 import './App.css';
-
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Routing from './Routing';
